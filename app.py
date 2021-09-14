26c7d93db7c686225f8d33618cad0dc631a70e36
poojitha0210 <84305950+poojitha0210@users.noreply.github.com>
2021-09-14 19:15:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import time
+import tweepy
+import os
+
+consumer_key=os.getenv('consumer_key')
+consumer_secret=os.getenv('consumer_secret')
+acess_token=os.getenv('access_token')
+access_token_secret=os.getenv('access_token_secret')
+
+auth=tweepy.
 
